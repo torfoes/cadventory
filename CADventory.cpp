48cf0d895afd7efa228b9833b08416d4712c3533
Christopher Sean Morrison <brlcad@gmail.com>
2024-02-11 14:27:32 -0500

    get the splash screen displaying

--- CADventory.cpp
+++ CADventory.cpp
@@ -1,5 +1,12 @@
 #include "CADventory.h"
 
+#include <QMainWindow>
+#include <QPixmap>
+#include <QSplashScreen>
+
+#include <unistd.h>
+
+
 CADventory::CADventory(int &argc, char *argv[]) : QApplication (argc, argv)
 {
   setOrganizationName("BRL-CAD");
@@ -8,6 +15,23 @@ CADventory::CADventory(int &argc, char *argv[]) : QApplication (argc, argv)
   setApplicationVersion("0.1.0");
 
   //  w = new MainWindow();
+  QPixmap pixmap("/Users/morrison/Desktop/RSEG127/cadventory/splash.png");
+  if (pixmap.isNull()) {
+    pixmap = QPixmap(512, 512);
+    pixmap.fill(Qt::white);
+  }
+  QSplashScreen splash(pixmap);
+  splash.show();
+  splash.showMessage("Loading... please wait.");
+  this->processEvents();
+
+  sleep(1);
+
+  QMainWindow window;
+  window.show();
+  this->processEvents();
+
+  //  splash.finish(&splash);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "CADventory.h"
 
+#include <QPixmap>
+#include <QSplashScreen>
+
+#include <iostream>
+#include <unistd.h>
+#include <filesystem>
+
 #ifdef _WIN32
 int APIENTRY
 WinMain(HINSTANCE hInstance,
@@ -15,5 +22,21 @@ main(int argc, char **argv)
 {
 #endif
   CADventory app(argc, argv);
+  //  QApplication app(argc, argv);
+
+  // std::cout << std::filesystem::current_path()<< std::endl;
+  QPixmap pixmap("../splash.png");
+  if (pixmap.isNull()) {
+    pixmap = QPixmap(512, 512);
+    pixmap.fill(Qt::white);
+  }
+  QSplashScreen splash(pixmap);
+  splash.show();
+  splash.showMessage("Loading from main... please wait.");
+  //  app.processEvents(QEventLoop::AllEvents);
+  app.processEvents();
+
+  sleep(1);
+
   return app.exec();
 }
