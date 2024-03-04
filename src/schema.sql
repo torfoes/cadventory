f3f7c469fc7ccb4d83f73dfb7987d85f918d1c53
Christopher Sean Morrison <brlcad@gmail.com>
2024-03-04 03:13:18 -0500

    create database sql schema

---
+++ src/schema.sql
@@ -0,0 +1,11 @@
+- SQLite database schema for CADventory
+
+-- Table for storing primary model information and overrides
+CREATE TABLE IF NOT EXISTS models (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    short_name TEXT NOT NULL UNIQUE, -- The folder name which is unique for each model
+    primary_cad_file TEXT, -- The path to the primary CAD file, can be relative to the short_name folder
+    override_info TEXT -- A JSON or text format to hold any overrides or additional information
+);
+
+-- Additional model info is stored in the model folder as metadata.json
