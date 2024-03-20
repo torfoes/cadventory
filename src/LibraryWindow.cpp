71c47388fce0286c8eb71ac5e0cf50526f9f60e0
Christopher Sean Morrison <brlcad@gmail.com>
2024-03-20 03:40:49 -0400

    try populating the list

--- src/LibraryWindow.cpp
+++ src/LibraryWindow.cpp
@@ -20,6 +20,16 @@ LibraryWindow::loadFromLibrary(Library* _library)
   this->library = _library;
   this->setWindowTitle(library->name() + QString(" Library"));
   this->ui.currentLibrary->setText(library->name());
+
+  /* populate the Models listing */
+  std::vector<std::string> modelDirs = library->getModels();
+  ui.listWidget->clear();
+
+  /* add each dir to the list */
+  for (const std::string& dir : modelDirs) {
+    QListWidgetItem* item = new QListWidgetItem(QString::fromStdString(dir));
+    ui.listWidget->addItem(item);
+  }
 }
 
 
